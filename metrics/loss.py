758f26f5a9080f29da1612d02c5ee3b840b7a9f4
SargamG <166918658+SargamG@users.noreply.github.com>
2025-06-12 13:12:09 +0530

    Update loss.py

--- metrics/loss.py
+++ metrics/loss.py
@@ -50,41 +50,15 @@ class PatchLoss(nn.Module):
         #return loss
         return loss_correct 
 
-def compute_trainloss(self, F):
+def compute_trainloss(self, F, omega = 1.2):
         """
         Compute the adaptive loss function
         """
-
-        #print(model_output.shape,true_labels.shape)
-        #print(model_output.argmax(dim=1).shape)
-              
-        # Get correctly classified and misclassified pixel sets
-        predict = torch.argmax(model_output, 1).float() + 1
-        target = label.float() + 1
-        target[target>=255] = 0
-        # print(predict.dtype,predict.shape,target.dtype,target.shape)
-        # temp1 = (predict == target).float()
-        # temp2 = (target>0).float()
-        # print(temp1.dtype,temp2.dtype)
-        correct_mask = (predict == target)*(target > 0)
-        incorrect_mask = (predict != target)*(target > 0)  # Opposite of correctly classified
-        #print(f'Correct mask: {correct_mask.shape}')  
-        # Compute separate loss terms
-        loss_correct = (loss_map * correct_mask).sum()/correct_mask.sum()  # Loss on correctly classified pixels
-        loss_incorrect = (loss_map * incorrect_mask).sum()/incorrect_mask.sum()  # Loss on already misclassified pixels
-
-        # Compute adaptive balancing factor
-        num_correct = correct_mask.sum()
-        num_total = (target != 0).sum()
-        gamma = num_correct / num_total  # Avoid division by zero
-
+        pos = F[F > 0].sum()
+        neg = F[F <= 0].sum()
         # Final adaptive loss
-        loss = gamma * loss_correct + (1 - gamma) * loss_incorrect
-        # print(f'Gamma:{gamma}')
-        # print(f'loss correct:{loss_correct}')
-        # print(f'loss incorrect: {loss_incorrect}')
-        #return loss
-        return loss_correct
+        loss = pos + (omega*neg)
+        return loss
 
     def compute_loss_direct(self, model_output, label):
         """
